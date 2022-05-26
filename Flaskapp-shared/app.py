426b33d55554d4a2538d19b4da5d1b188de7e8c6
Dylan van Gog <>
2022-05-26 16:59:07 +0200

    Working shared web app

--- Flaskapp-shared/app.py
+++ Flaskapp-shared/app.py
@@ -1,31 +1,41 @@
 import csv
 import os
-from flask import Flask, render_template, url_for, request, redirect
+import requests
+from flask import Flask, render_template, url_for, request, redirect, session
+from flask_session import Session 
 from azure.servicebus import ServiceBusClient, ServiceBusMessage
 from azure.keyvault.secrets import SecretClient
 from azure.identity import DefaultAzureCredential
+import msal
+import app_config
+
 app = Flask(__name__)
 
+app.config.from_object(app_config)
+Session(app)
+
+
+# Old keyvault code.
 
 # Azure KeyVault name + URL
 keyVaultName = "KeyVaultMixit"
 KVUri = f"https://{keyVaultName}.vault.azure.net"
 
-credential = DefaultAzureCredential()
-client = SecretClient(vault_url=KVUri, credential=credential)
+#credential = DefaultAzureCredential()
+#client = SecretClient(vault_url=KVUri, credential=credential)
 
 # Get secrets from keyvault "KeyVaultMixit" for acces to servicebus.
-retrieved_secret_textdatafromwebapp = client.get_secret("WebAppKeyQuetextdatafromwebapp")
-QUEUE_NAME_send = "textdatafromwebapp"
+#retrieved_secret_textdatafromwebapp = client.get_secret("WebAppKeyQuetextdatafromwebapp")
+#QUEUE_NAME_send = "textdatafromwebapp"
 
-retrieved_secret_quetowebapp = client.get_secret("WebAppKeyQuequetowebapp")
-QUEUE_NAME_receive = "quetowebapp"
+#retrieved_secret_quetowebapp = client.get_secret("WebAppKeyQuequetowebapp")
+#QUEUE_NAME_receive = "quetowebapp"
 
-retrieved_secret_fromwebappwheatherdata = client.get_secret("WebAppKeyQuefromwebappwheatherdata")
-QUEUE_NAME_WeatherAPISendQue = "fromwebappwheatherdata"
+#retrieved_secret_fromwebappwheatherdata = client.get_secret("WebAppKeyQuefromwebappwheatherdata")
+#QUEUE_NAME_WeatherAPISendQue = "fromwebappwheatherdata"
 
-retrieved_secret_fromweerapptowebapp = client.get_secret("WebAppKeyQuefromweerapptowebapp")
-QUEUE_NAME_WeatherAPIReceiveQue = "fromweerapptowebapp"
+#retrieved_secret_fromweerapptowebapp = client.get_secret("WebAppKeyQuefromweerapptowebapp")
+#QUEUE_NAME_WeatherAPIReceiveQue = "fromweerapptowebapp"
 
 # For troubleshooting key vault
 #print(retrieved_secret_textdatafromwebapp.value)
@@ -33,14 +43,18 @@ QUEUE_NAME_WeatherAPIReceiveQue = "fromweerapptowebapp"
 #print(retrieved_secret_fromwebappwheatherdata.value)
 #print(retrieved_secret_fromweerapptowebapp.value)
 
-# Render default page
-@app.route('/')
-def homepage():
-    return render_template('index.html')
+# Website redirects
 
 # When user use button "Verzenden" on webpage index.html then it enters this function
-@app.route('/', methods=['POST'])
-def UserInputDataField():
+@app.route('/')
+def index():
+    if not session.get("user"):
+        return redirect(url_for("login"))
+    return render_template('index.html', user=session["user"], version=msal.__version__)
+    
+    
+    # Old code to servicebus and other things 
+
     # User data goes into variable
     UserInputData = request.form.get("UserInputDataFromField")
 
@@ -60,7 +74,49 @@ def UserInputDataField():
 
     # Return index.html and new variable from servicebus
     return render_template("index.html", UserInputData=UserInputData, weerAppReturnData=weerAppReturnData)
-    
+
+@app.route("/login")
+def login():
+    session["flow"] = _build_auth_code_flow(scopes=app_config.SCOPE)
+    return render_template("login.html", auth_url=session["flow"]["auth_uri"], version=msal.__version__)
+
+@app.route("/graphcall")
+def graphcall():
+    token = _get_token_from_cache(app_config.SCOPE)
+    if not token:
+        return redirect(url_for("login"))
+    print(token)
+    #Ophalen data met graph API
+    graph_data = requests.get(
+        app_config.CENDPOINT,
+        headers={'Authorization': 'Bearer ' + token['access_token']},
+        ).json()['value']
+    print(graph_data)
+    return render_template('schedule.html', data=graph_data)
+
+@app.route("/logout")
+def logout():
+    session.clear()
+    return redirect(
+        app_config.AUTHORITY + "/oauth2/v2.0/logout" +
+        "?post_logout_redirect_uri=" + url_for("index", _external=True))
+
+@app.route(app_config.REDIRECT_PATH)  # Applicatie URI 
+def authorized():
+    try:
+        cache = _load_cache()
+        result = _build_msal_app(cache=cache).acquire_token_by_auth_code_flow(
+            session.get("flow", {}), request.args)
+        if "error" in result:
+            return render_template("auth_error.html", result=result)
+        session["user"] = result.get("id_token_claims")
+        _save_cache(cache)
+    except ValueError:
+        pass
+    return redirect(url_for("index"))
+
+# Send data to servicebus bellow
+
 # Code for send a single message for the fromweerapptowebapp.
 def send_single_message_to_weatherfunction_que(UserText):
     # retrieved_secret_fromwebappwheatherdata.value get the plaintext value from the secret
@@ -95,3 +151,38 @@ def received_single_message():
                 bodyMessage = "Receiving: {}".format(message)
                 receiver.complete_message(message)
                 return(bodyMessage)
+
+
+# Team 2 code bellow
+
+
+def _load_cache():
+    cache = msal.SerializableTokenCache()
+    if session.get("token_cache"):
+        cache.deserialize(session["token_cache"])
+    return cache
+
+def _save_cache(cache):
+    if cache.has_state_changed:
+        session["token_cache"] = cache.serialize()
+
+def _build_msal_app(cache=None, authority=None):
+    return msal.ConfidentialClientApplication(
+        app_config.CLIENT_ID, authority=authority or app_config.AUTHORITY,
+        client_credential=app_config.CLIENT_SECRET, token_cache=cache)
+
+def _build_auth_code_flow(authority=None, scopes=None):
+    return _build_msal_app(authority=authority).initiate_auth_code_flow(
+        scopes or [],
+        redirect_uri=url_for("authorized", _external=True))
+
+def _get_token_from_cache(scope=None):
+    cache = _load_cache()
+    cca = _build_msal_app(cache=cache)
+    accounts = cca.get_accounts()
+    if accounts:
+        result = cca.acquire_token_silent(scope, account=accounts[0])
+        _save_cache(cache)
+        return result
+
+app.jinja_env.globals.update(_build_auth_code_flow=_build_auth_code_flow)  # Used in template
