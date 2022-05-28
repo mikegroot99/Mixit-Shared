d638d728014b3fec8d61d79053175845a6f49d32
Brian Dekker <Brian.dekker@hva.nl>
2022-05-28 16:53:22 +0200

    function app

--- FunctionApps/FunctionApps/ServiceBusQueueTrigger1/__init__.py
+++ FunctionApps/FunctionApps/ServiceBusQueueTrigger1/__init__.py
@@ -1,5 +1,6 @@
 from http.client import responses
 import logging
+import string
 import requests
 import json
 import azure.functions as func
@@ -8,20 +9,15 @@ import azure.functions as func
 #The inputRequest is the trigger input
 #The outputWeatherAPI is the output these are defined in the function.json
 def main(inputRequest: func.ServiceBusMessage,
-         outputWeatherAPI: func.Out[str]):
+         outputOutlookApi: func.Out[str]):
     # Log the input so we can see what it gets from the queue
     logging.info('Python ServiceBus queue trigger processed message: %s',
                  inputRequest.get_body().decode('utf-8'))
     #Create the request URL for the weather API with the input from the queue
     message =  inputRequest.get_body().decode('utf-8')
-    api_url = "https://api.openweathermap.org/data/2.5/weather?q=" + message + "&appid=a6c60d1395554bbac92f34b7ebf8122d"
-    #Request data from weather API with URL
-    response = requests.get(api_url)
-    #Log the response of the Weather API so we can see what it sends back
-    logging.info(response.json())
+    token, request = message.split(';')
+    graph_data = requests.get(request, headers={'Authorization': 'Bearer ' + token},).json()
     #Convert json to a string so that we can send it back to the second queue
-    data = json.loads(response.text)
-    jsonToString = json.dumps(data)
     #Set the value of output to the json to send back
-    outputWeatherAPI.set(jsonToString)
+    outputOutlookApi.set(str(graph_data))
     #wtf
