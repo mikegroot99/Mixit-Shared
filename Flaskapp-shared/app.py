4a775fe1412a2729aa2b7b29886c0ee18b66c929
Osman <aytac_osman1994@hotmail.com>
2022-05-29 16:58:24 +0200

    Web app deployment gelukt

--- Flaskapp-shared/app.py
+++ Flaskapp-shared/app.py
@@ -15,6 +15,9 @@ app = Flask(__name__)
 app.config.from_object(app_config)
 Session(app)
 
+from werkzeug.middleware.proxy_fix import ProxyFix
+app.wsgi_app = ProxyFix(app.wsgi_app, x_proto=1, x_host=1)
+
 
 # serviceque connection strings
 # function app (brian) azure location
