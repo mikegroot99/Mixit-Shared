7c2ed90361747e627a7df3ec4d61454ea8fd6f28
Brian Dekker <briandekk@gmail.com>
2022-05-30 13:39:54 +0200

    Added comments to function app

--- FunctionApps/FunctionApps/ServiceBusQueueTrigger1/__init__.py
+++ FunctionApps/FunctionApps/ServiceBusQueueTrigger1/__init__.py
@@ -14,12 +14,13 @@ def main(inputRequest: func.ServiceBusMessage,
          outputOutlookApi: func.Out[str]):
     logging.info('\n-->AZURE FUNCTION APP')           
     message =  inputRequest.get_body().decode('utf-8')
-    
+    #Split the message based on ; and put the results in variable: token and request
     token, request = message.split(';')
     #graph_data = requests.get(request, headers={'Authorization': 'Bearer ' + token}).json()['value']<-- Dit split de agenda punten op 
     graph_data = requests.get(request, headers={'Authorization': 'Bearer ' + token}).json()
-
+    #Transform the json we get from the graph api to a string  
     testjson = json.dumps(graph_data)
     logging.info('Dit is wat er uitkomt: ' + testjson)
+    #Set response from graph api into the output.
     outputOutlookApi.set(testjson)
 
