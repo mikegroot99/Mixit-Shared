00d584e71e6c7c42b41af8de21e554bb81b05122
Brian Dekker <Brian.dekker@hva.nl>
2022-05-31 22:42:55 +0200

    clean code

--- FunctionApps/FunctionApps/ServiceBusQueueTrigger1/__init__.py
+++ FunctionApps/FunctionApps/ServiceBusQueueTrigger1/__init__.py
@@ -8,21 +8,14 @@ import azure.functions as func
 
 def main(inputRequest: func.ServiceBusMessage,
          outputOutlookApi: func.Out[str]):
-    logging.info('\n-->AZURE FUNCTION APP')  
     #Take the input and decode it to a normal string value.         
     message =  inputRequest.get_body().decode('utf-8')
-
     #Split the message based on ; and put the results in variable: token and request.
     token, request = message.split(';')
-
-    #graph_data = requests.get(request, headers={'Authorization': 'Bearer ' + token}).json()['value']<-- This splits the output into multiple messages.
     #Use to token and request variable to communicate with the graph api.
     graph_data = requests.get(request, headers={'Authorization': 'Bearer ' + token}).json()
-
     #Transform the json we get from the graph api to a string. 
     testjson = json.dumps(graph_data)
-    logging.info('Dit is wat er uitkomt: ' + testjson)
-
     #Set response from graph api into the output.
     outputOutlookApi.set(testjson)
 
