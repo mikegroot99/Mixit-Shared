36dba673b75bed5696b72ef2583e6f3d7fb9f839
Brian Dekker <briandekk@gmail.com>
2022-06-01 14:16:03 +0200

    yep

--- FunctionApps/FunctionApps/jaivyfunctionapp1/__init__.py
+++ FunctionApps/FunctionApps/jaivyfunctionapp1/__init__.py
@@ -6,4 +6,4 @@ import azure.functions as func
 def main(inputGraphApi: func.ServiceBusMessage, outputGraphApi: func.Out[str]):
     logging.info('Python ServiceBus queue trigger processed message: %s',
                  inputGraphApi.get_body().decode('utf-8'))
-    outputGraphApi.set(inputGraphApi + 'Dit is een aanpassing')
+    outputGraphApi.set(inputGraphApi.get_body().decode('utf-8') + ' cloud solutions')
